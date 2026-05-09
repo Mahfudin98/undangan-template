b1090e9e09954748f3d4ce9a6d0e1e86cf7bf891
Mahfudin98 <mahfudinnawawi@gmail.com>
2026-05-09 15:10:02 +0700

    fixing next.config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   images: {
-    unoptimized: true,
     domains: [
       "lh3.googleusercontent.com",
       "invitation.lsskincare.id", // ✅ tambahkan domain production
